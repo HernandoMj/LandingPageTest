325f3ef495e7c7e28b26fdbc1e65d38829c6f295
HernandoMj <jm1854040@gmail.com>
2025-02-14 18:00:43 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -99,5 +99,5 @@ const typeWriter = (element, text, i = 0) => {
 
         // Desaparecer y reaparecer algunas fotos
         fadeInOutRandom(photos);
-    }, 15000); // Cada 10 segundos
+    }, 10000); // Cada 10 segundos
 });
